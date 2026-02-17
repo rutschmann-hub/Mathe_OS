47d6588caf05ae8c96b86f7d6704b5be162505c8
rutschmann-hub <rutschmann@posteo.de>
2026-02-17 21:56:21 +0100

    Add startup configuration for MathJax

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -6,5 +6,14 @@ window.MathJax = {
     },
     options: {
         skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+    },
+    startup: {
+        ready() {
+            MathJax.startup.defaultReady();
+            MathJax.startup.document.menu.settings.inTabOrder = false;
+        }
+    },
+    chtml: {
+        displayAlign: 'center'
     }
 };
